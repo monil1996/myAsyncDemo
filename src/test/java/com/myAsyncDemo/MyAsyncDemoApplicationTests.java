545b763ce639caafe821fa08b7adbcfe5a7356aa
Monil Ghadiya <monil.ghadiya@gmail.com>
2020-02-13 19:35:39 +0530

    initial commit

---
+++ src/test/java/com/myAsyncDemo/MyAsyncDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myAsyncDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAsyncDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
